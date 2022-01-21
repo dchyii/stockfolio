3e5820b145d95d5d7a0c38a7f8bfd8552b9929db
dchyii <hongyichong@gmail.com>
2022-01-21 16:34:19 +0800

    fetch all price data on application load

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -1,10 +1,13 @@
-import watchlistData from "../Data/watchlistData";
-import watchlistStocks from "../Data/watchlistStocks";
+// import watchlistData from "../Data/watchlistData";
+// import watchlistStocks from "../Data/watchlistStocks";
+import { useOutletContext } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 function Watchlist() {
-  console.log("watchlist stock", watchlistStocks);
-  console.log("watchlist", watchlistData);
+  // console.log("watchlist stock", watchlistStocks);
+  // console.log("watchlist", watchlistData);
+  const priceData = useOutletContext();
+  console.log(priceData);
   return (
     <>
       <h2>Hello Watchlist</h2>
