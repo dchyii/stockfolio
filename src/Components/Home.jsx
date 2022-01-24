79f6f1c8ac32613d72c63a1746cb7833ca45fa6f
dchyii <hongyichong@gmail.com>
2022-01-24 10:50:35 +0800

    updated code to fetch all price data on application load

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,33 +1,49 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import dayjs from "dayjs";
+import allPriceData from "../Data/allPriceData";
 import SearchBar from "./SearchBar";
 
 const KEY = process.env.REACT_APP_APIKEY;
-const urlPreviousDayClose = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-01-20?adjusted=true&apiKey=${KEY}`;
 
 function Home() {
-  const [priceData, setPriceData] = useState(["test"]);
-
-  const today = dayjs();
-  const date = today.subtract(1, "day");
-  console.log("today", today.format("YYYY-MM-DD"));
-  console.log("yesterday", date.format("YYYY-MM-DD"));
-
-  const fetchPriceData = () => {
-    console.log("fetching price data");
-    // fetch(urlPreviousDayClose)
-    //   .then((response) => {
-    //     console.log("processing price data");
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     console.log("price data fetched");
-    //     setPriceData(data.results);
-    //   });
-  };
-
+  const [priceData, setPriceData] = useState({ date: dayjs(), data: ["test"] });
   useEffect(() => {
+    let fetchDate = dayjs();
+
+    const fetchPriceData = () => {
+      if (fetchDate.get("day") === 0 || fetchDate.get("day") === 6) {
+        console.log("dont fetch", fetchDate.format("YYYY-MM-DD"));
+        fetchDate = fetchDate.subtract(1, "day");
+        fetchPriceData();
+      } else {
+        //! vvv uncomment on production vvv !//
+        // let formattedFetchDate = fetchDate.format("YYYY-MM-DD");
+        // let urlPreviousDayClose = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${formattedFetchDate}?adjusted=true&apiKey=${KEY}`;
+        // console.log("fetching price data", formattedFetchDate);
+        // fetch(urlPreviousDayClose)
+        //   .then((response) => {
+        //     console.log("processing price data");
+        //     return response.json();
+        //   })
+        //   .then((data) => {
+        //     if (data.queryCount === 0) {
+        //       console.log("zero data");
+        //       fetchDate = fetchDate.subtract(1, "day");
+        //       fetchPriceData();
+        //     } else {
+        //       console.log("price data fetched");
+        //       setPriceData({ date: fetchDate, data: data.results });
+        //     }
+        // });
+        //! ^^^ uncomment on production ^^^ !//
+        //! vvv delete on production vvv !//
+        console.log("simulated data");
+        setPriceData({ date: fetchDate, data: allPriceData });
+        //! ^^^ delete on production ^^^ !//
+      }
+    };
+
     fetchPriceData();
   }, []);
 
