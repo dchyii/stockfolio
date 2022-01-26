5b2b1a0a905cb19157ef08b05b9f110458cfa782
dchyii <hongyichong@gmail.com>
2022-01-26 11:05:11 +0800

    added elvis operator to load on refresh

--- src/Components/BigInfoCard.jsx
+++ src/Components/BigInfoCard.jsx
@@ -10,33 +10,33 @@ function BigInfoCard(props) {
       className="outline outline-1 outline-orange-400 rounded-md h-fit p-3 bg-orange-50"
     >
       <img
-        src={tickerDetails.branding.logo_url + `?apiKey=${key}`}
+        src={tickerDetails?.branding?.logo_url + `?apiKey=${key}`}
         alt="company logo"
         className="h-24 mx-auto my-3"
       />
       <div className="flex justify-between text-xl font-bold">
         <h2 className="text-orange-400">
-          {tickerDetails.name} ({tickerDetails.ticker})
+          {tickerDetails?.name} ({tickerDetails?.ticker})
         </h2>
-        <h2>${tickerPrice.c}</h2>
+        <h2>${tickerPrice?.c}</h2>
       </div>
       <p className="my-3 text-left">
-        {tickerDetails.address.address1}
+        {tickerDetails?.address?.address1}
         <br />
-        {tickerDetails.address.city}
+        {tickerDetails?.address?.city}
         <br />
-        {tickerDetails.address.state} {tickerDetails.address.postal_code}
+        {tickerDetails?.address?.state} {tickerDetails?.address?.postal_code}
         <br />
         <a
-          href={tickerDetails.homepage_url}
+          href={tickerDetails?.homepage_url}
           className="hover:text-blue-500 hover:underline"
         >
-          {tickerDetails.homepage_url}
+          {tickerDetails?.homepage_url}
         </a>
       </p>
-      <p className="my-3 text-left">Market Cap: {tickerDetails.market_cap}</p>
+      <p className="my-3 text-left">Market Cap: {tickerDetails?.market_cap}</p>
 
-      <p className="my-3 text-justify">{tickerDetails.description}</p>
+      <p className="my-3 text-justify">{tickerDetails?.description}</p>
     </div>
   );
 }
