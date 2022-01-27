943b66158f97c5d7bd33185bf6cf84d47afa3028
dchyii <hongyichong@gmail.com>
2022-01-27 17:38:24 +0800

    updated css for search bar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -10,19 +10,34 @@ function SearchBar(props) {
   };
 
   return (
-    <div id="SearchBar" className="w-10/12 mx-auto py-2 ">
-      <input
-        type="text"
-        placeholder="Enter Ticker Symbol"
-        className="w-4/5 bg-gray-200 outline outline-1 outline-gray-400 focus:outline-gray-800 text-gray-800 px-2 rounded"
-        ref={searchRef}
-      />
-      <button
-        className="bg-gray-300 outline outline-1 outline-gray-400 text-gray-800 px-2 mx-2 rounded hover:outline-orange-800 hover:bg-orange-400 hover:text-slate-100"
-        onClick={navToSearchResult}
-      >
-        Search
-      </button>
+    <div id="SearchBar" className="w-10/12 mx-auto py-5">
+      <div className="w-full mx-auto relative">
+        <input
+          type="text"
+          placeholder="Search Ticker Symbol"
+          className="w-full border-2 px-3 border-gray-300 focus:outline-orange-400 bg-white h-10 rounded-lg focus:shadow-lg"
+          ref={searchRef}
+        />
+        <button
+          className="absolute right-0 top-0 mt-2 mr-3"
+          onClick={navToSearchResult}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-slate-700 hover:text-orange-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
